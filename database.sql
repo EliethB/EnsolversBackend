d93587806a08eb510c9d02c0b4996b52f73fadf7
EliethB <eliethbarquero01@gmail.com>
2024-02-18 22:43:37 -0600

    Ultimos cambios

--- database.sql
+++ database.sql
@@ -6,12 +6,20 @@ CREATE TABLE IF NOT EXISTS state (
     status VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nameCategory VARCHAR(100) NOT NULL
+ );
+
 CREATE TABLE IF NOT EXISTS notes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(1000) NOT NULL,
     number INT, 
     id_state INT NOT NULL,
-    FOREIGN KEY (id_state) REFERENCES state(id)
+    id_category INT,
+    FOREIGN KEY (id_state) REFERENCES state(id),
+	FOREIGN KEY (id_category) REFERENCES categories(id)
 );
 INSERT INTO ensolvers_data.state (id, status) VALUES ('1', 'active');
-INSERT INTO ensolvers_data.state (id, status) VALUES ('2', 'archive');
+INSERT INTO ensolvers_data.state (id, status) VALUES ('2', 'archived');
+
